0fd32f1350a6410791d230d3e108e50d69f79a5e
tituswe <tituslowesg@gmail.com>
2023-02-23 18:07:02 +0800

    Complete Project: Ravenous Part I:

---
+++ ravenous/src/components/App/App.js
@@ -0,0 +1,15 @@
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<h1>ravenous</h1>
+			<SearchBar />
+			<BusinessList />
+		</div>
+	);
+}
+
+export default App;
